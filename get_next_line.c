9ff5e99ed4164074daa1335a9cb03c0325295b75
Nico <ndubouilh@gmail.com>
2018-01-04 01:09:21 +0100

    start cleaning

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ndubouil <ndubouil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/14 08:47:49 by ndubouil          #+#    #+#             */
-/*   Updated: 2017/12/21 20:00:42 by ndubouil         ###   ########.fr       */
+/*   Updated: 2018/01/04 01:08:50 by ndubouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,10 +51,7 @@ t_list		*ft_lstsearch(t_list **lst, int fd)
 	while (tmp != NULL)
 	{
 		if (T_FILE_FD == fd)
-		{
-			//ft_putendl("fd trouve");
 			return (tmp);
-		}
 		tmp = tmp->next;
 	}
 	if (!(tmp = ft_lstnew(NULL, sizeof(t_file *))))
@@ -74,115 +71,36 @@ int		get_next_line(const int fd, char **line)
 	char			*temp;
 	int				opt;
 
-	if (BUFF_SIZE <= 0 || fd == 1 || (n_lus = read(fd, "", 0)) == -1)
+	// Erreurs
+	if (BUFF_SIZE <= 0 | fd == 1 | (n_lus = read(fd, "", 0)) == -1)
 		return (-1);
-	if (!(buff = ft_strnew(BUFF_SIZE)))
+	// Creation du buffer et du maillon
+	if (!(buff = ft_strnew(BUFF_SIZE)) || !(tmp = ft_lstsearch(&lst, fd)))
 		return (-1);
-	tmp = ft_lstsearch(&lst, fd);
-	//if (T_FILE_BUFF == NULL)
-	//	ft_putendl("null");
-	//T_FILE_BUFF = ft_strjoin(T_FILE_BUFF, "test");
-	//ft_putstr(T_FILE_BUFF);
+	// tant qu'il reste du texte, soit dans le buffer de la liste soit dans le fichier
 	while (!ft_strchr(T_FILE_BUFF, '\n') && (n_lus = read(fd, buff, BUFF_SIZE) > 0))
 	{
+		// Obligatoire pour free l'ancien
 		temp = T_FILE_BUFF;
-		//ft_putendl(temp);
+		// Concatene le buffer de la liste avec ce qui a ete lu
 		T_FILE_BUFF = ft_strjoin(T_FILE_BUFF, buff);
-		//ft_putendl(T_FILE_BUFF);
 		ft_strdel(&temp);
-		ft_memset(buff, 0, BUFF_SIZE);
+		ft_strclr(buff);
 	}
 	ft_strdel(&buff);
-	//if (n_lus == 0 && T_FILE_BUFF == '\0')
-//	ft_putendl("je passe ici");
 	*line = ft_strsub(T_FILE_BUFF, 0, ft_strpos(T_FILE_BUFF, '\n'));
-	if (n_lus == 0 && *T_FILE_BUFF == 0)//ft_strchr(T_FILE_BUFF, '\n') == NULL)
+	if (n_lus == 0 && *T_FILE_BUFF == 0)
 	{
 		ft_memdel((void **)&T_FILE_BUFF);
-		ft_memdel(&tmp->content);
-		//T_FILE_BUFF = ft_strnew(0);
-		/*ft_putendl("Etat du buffer du maillon :");
-		ft_putendl(T_FILE_BUFF);
-		ft_putendl("fichier termine !");*/
-		//ft_memdel((void **)&T_FILE_BUFF);
-		//T_FILE_BUFF = ft_strnew(0);
-		//*line = ft_strsub(T_FILE_BUFF, 0, ft_strpos(T_FILE_BUFF, '\0'));
+		ft_strclr(tmp->content);
 		return (0);
 	}
 	if (ft_strpos(T_FILE_BUFF, '\n') == (int)ft_strlen(T_FILE_BUFF))
 		opt = 0;
 	else
 		opt = 1;
-//	ft_putendl("je passe la");
-	//ft_putendl(*line);
-	//T_FILE_BUFF = ft_strchr(T_FILE_BUFF, '\n');
 	temp = T_FILE_BUFF;
 	T_FILE_BUFF = ft_strsub(T_FILE_BUFF, ft_strpos(T_FILE_BUFF, '\n') + opt, (int)ft_strlen(T_FILE_BUFF));
 	ft_strdel(&temp);
-	/*ft_putendl("Etat du buffer du maillon :");
-	ft_putendl(T_FILE_BUFF);
-	ft_putendl("FINI");*/
 	return (1);
 }
-
-/*
-int		get_next_line(const int fd, char **line)
-{
-	static t_list	*lst;
-	int				n_lus;
-	//char			tmp[BUFF_SIZE];
-	char			*tmp;
-	char			*result;
-	int				pos;
-	char			*temp;
-	t_list			*tmp_lst;
-
-	if (!(tmp_lst = ft_lstsearch(lst, fd)))
-	{
-		if (lst == NULL)
-		{
-			if (!(lst = ft_lstnew(ft_newfile(fd), sizeof(t_file *))))
-				return (-1);
-		}
-		else
-			ft_lstaddend(&lst, ft_lstnew(ft_newfile(fd), sizeof(t_file *)));
-		tmp_lst = ft_lstsearch(lst, fd);
-	}
-	result = ft_strnew(0);
-	tmp = ft_strnew(sizeof(char) * BUFF_SIZE);
-	if (((t_file *)(tmp_lst->content))->buff != NULL)
-	{
-		ft_putnbr((int)ft_strlen(((t_file *)(tmp_lst->content))->buff));
-		tmp = ft_strjoin(tmp, ((t_file *)(tmp_lst->content))->buff);
-		ft_memdel((void **)(&((t_file *)(tmp_lst->content))->buff));
-	}
-
-	while ((n_lus = read(fd, tmp, BUFF_SIZE)) > 0)
-	{
-		// Si il y a un '\n' dans le buffer
-		if ((pos = ft_strmatch(tmp, '\n')) >= 0)
-		{
-			// recupere le troncon jusqu'au '\n'
-			temp = ft_strsub(tmp, 0, pos);
-			// l'ajoute au resultat
-			result = ft_strjoin(result, temp);
-			// free le temp
-			//ft_strdel(&temp);
-			// Passe le \n
-			read(fd, "", 1);
-			// ajoute le reste du buffer dans la liste
-			if (!(((t_file *)(tmp_lst->content))->buff = ft_strsub(tmp, pos + 1, BUFF_SIZE)))
-				return (-1);
-			break;
-		}
-		//temp = ft_strsub(tmp, 0, n_lus);
-		result = ft_strjoin(result, tmp);
-		//ft_strdel(&temp);
-		// Fin de fichier
-		if (n_lus < BUFF_SIZE)
-			break;
-	}
-	*line = result;
-	ft_putstr(result);
-	return (0);
-}*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ndubouil <ndubouil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/14 08:37:46 by ndubouil          #+#    #+#             */
-/*   Updated: 2017/12/21 17:28:30 by ndubouil         ###   ########.fr       */
+/*   Updated: 2017/12/29 18:11:45 by ndubouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "libft/libft.h"
 
-# define BUFF_SIZE 10
+# define BUFF_SIZE 9999
 # define T_FILE_BUFF ((t_file *)(tmp->content))->buff
 # define T_FILE_FD ((t_file *)(tmp->content))->fd
 
